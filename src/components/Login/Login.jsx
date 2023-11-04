3d6db2c621b3528d97abefc408273172620d2819
Ritesh Kumar <singlaritesh007@gmail.com>
2023-11-04 18:25:31 +0530

    navbar

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import styles from "./Login.module.scss"
+import NavBar from "../NavBar/NavBar"
+
+const Login = () => {
+  return (
+    <div className={styles.loginContainer}>
+      <NavBar />
+    </div>
+  )
+}
+
+export default Login
