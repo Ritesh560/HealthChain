e65080ce4784923c878383d4b3512c1cd1fa73d9
Bharat Kumar <kumarbharat661@gmail.com>
2023-11-05 10:40:39 +0530

    changed to seopli

--- src/ethereum/hardhat.config.js
+++ src/ethereum/hardhat.config.js
@@ -1,14 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
 
 const endpointUrl =
-  "https://eth-goerli.g.alchemy.com/v2/dbA10pRRMHKEDsW9NIzSzilyOf_PCd3i";
+  "https://eth-sepolia.g.alchemy.com/v2/LrMc5RVmPcXfwJ2WK9mYjkYs9QUdKoDI";
 const privateKey =
   "72243dee323acf5d072dfb423ce58266f66e12add974cb586aeef172469ac656";
 
 module.exports = {
   solidity: "0.8.21",
   networks: {
-    goerli: {
+    seopli: {
       url: endpointUrl,
       accounts: [privateKey],
     },
